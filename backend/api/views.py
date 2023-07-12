3ddd1a41d06bbb08d01df5e05b880ba8470be7dc
Наталья Столярова <semechkina.n@yandex.ru>
2023-07-12 16:15:22 +0300

    old views and filters version

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
-from django.db.models import Count, Sum
+# from django.db.models import Count, Sum
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
@@ -62,7 +63,7 @@ class CustomUserViewSet(UserViewSet):
         favorites = user.followers.all()
         users_id = [favorite_instance.author.id
                     for favorite_instance in favorites]
-        users = User.objects.filter(id__in=users_id)
+        # users = User.objects.filter(id__in=users_id)
         # конец
         # favorites = User.objects.filter(followings__user=self.request.user)
         paginated_queryset = self.paginate_queryset(favorites)
