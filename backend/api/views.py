8b01a9fa01bc7e9b228c130ac6a947d96e920cda
Наталья Столярова <semechkina.n@yandex.ru>
2023-07-13 11:05:42 +0300

    new queryset of recipe

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,8 +6,8 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 
 from django.contrib.auth import get_user_model
-from django.db.models import Count, Sum
-# from django.db.models import Sum
+# from django.db.models import Count, Sum
+from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 
